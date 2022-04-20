ea155c051d2ebe90f1385c746ff9b83613c83c77
Angel-Juarez <angelnjuarez@gmail.com>
2022-04-19 23:16:34 -0300

    Calculadora cuadrado

--- figuras.js
+++ figuras.js
@@ -4,12 +4,10 @@ console.group("Cuadrados");
 function perimetroCuadrado(lado){
     return lado * 4;
 };
-//console.log("El perímetro del cuadrado es: " + perimetroCuadrado + " cm");
 
 function areaCuadrado(lado){
     return lado * lado;
 };
-//console.log("El área del cuadrado es: " + areaCuadrado + " cm^2");
 console.groupEnd();
 
 //Codigo triangulo
@@ -18,18 +16,14 @@ console.group("Triangulos");
 function perimetroTriangulo(lado1, lado2, base){
     return lado1 + lado2 + base;
 };
-//console.log("El perímetro del triangulo es: " + perimetroTriangulo + " cm");
-
 
 function areaTriangulo(base, altura){
     return (base * altura)/2;
 };
-//console.log("El área del triangulo es: " + areaTriangulo + " cm^2");
 console.groupEnd();
 
 //Codigo del circulo
 console.group("Circulos");
-
 
 function diametroCirculo(radio){
     return radio*2;
@@ -38,10 +32,24 @@ function diametroCirculo(radio){
 function perimetroCirculo(radio){
     return diametroCirculo(radio) * Math.PI;
 };
-//console.log("El perímetro del circulo es: " + perimetroCirculo + " cm");
 
 function areaCirculo(radio){
     return (radio*radio)*Math.PI;
 };
-//console.log("El área del circulo es: " + areaCirculo + " cm^2");
 console.groupEnd();
+
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado(){
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area)
+}
