d6727d7a03a1ee3adec80c4189e099c7d285d618
Angel-Juarez <angelnjuarez@gmail.com>
2022-04-23 18:54:20 -0300

    Se agragaron los calculos del triangulo

--- figuras.js
+++ figuras.js
@@ -53,3 +53,27 @@ function calcularAreaCuadrado(){
     const area = areaCuadrado(value);
     alert(area)
 }
+
+function calcularPerimetroTriangulo(){
+    const input1 = document.getElementById("inputTriangulo1");
+    const input2 = document.getElementById("inputTriangulo2");
+    const input3 = document.getElementById("inputTrianguloBase");
+
+    const lado1 = Number(input1.value);
+    const lado2 = Number(input2.value);
+    const base = Number(input3.value);
+
+    const perimetro = perimetroTriangulo(lado1, lado2, base);
+    alert(perimetro)
+}
+
+function calcularAreaTriangulo(){
+    const input1 = document.getElementById("inputTrianguloAltura");
+    const input2 = document.getElementById("inputTrianguloBase");
+
+    const altura = Number(input1.value);
+    const base = Number(input2.value);
+
+    const perimetro = areaTriangulo(base, altura);
+    alert(perimetro)
+}
