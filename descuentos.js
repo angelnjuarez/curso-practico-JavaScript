5c3d0d0d9dc9f901578401aea21bfaac8bfc61f5
Angel-Juarez <angelnjuarez@gmail.com>
2022-04-26 18:28:13 -0300

    Se cambia el cupon por un array de objetos, y se modifican las funciones

--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,18 @@
+const cupones = [
+    {
+       name : "Jujutsu",
+       descuento : 15,
+    },
+    {
+        name : "Chainsawman",
+        descuento : 30,
+    },
+    {
+        name : "Haikyuu",
+        descuento : 25,
+    },
+];
+
 function calcularPrecioConDescuento(precio, descuento){
     const porcentajeConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajeConDescuento) /100;
@@ -9,10 +24,23 @@ function obtenerPrecioConDescuento(){
     const inputPrecio = document.getElementById("inputPrecio");
     const precio = inputPrecio.value;
 
-    const inputDescuento = document.getElementById("inputDescuento");
-    const descuento = inputDescuento.value;
+    const inputCupon = document.getElementById("inputCupon");
+    const valorCupon = inputCupon.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(precio, descuento);
-    const resultadoP = document.getElementById("ResultadoPrecio");
-    resultadoP.innerText = "El precio con descuento es: $" + precioConDescuento;
+
+    const esValorCuponValido = function(cupon){
+        return cupon.name === valorCupon;
+    }
+
+    const cuponUsuario = cupones.find(esValorCuponValido);
+
+    if(!cuponUsuario){
+        alert("El cupón " + valorCupon + " no es válido.")
+    }else{
+        const descuento = cuponUsuario.descuento;
+        const precioConDescuento = calcularPrecioConDescuento(precio, descuento);
+
+        const resultadoP = document.getElementById("ResultadoPrecio");
+        resultadoP.innerText = "El precio con descuento es: $" + precioConDescuento;
+    }
 }
